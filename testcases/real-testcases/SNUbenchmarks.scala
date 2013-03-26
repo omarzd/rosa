23cdaba4c137456a0ee6c0b1fdf6d5ebb27f8782
Eva Darulova <eva.darulova@epfl.ch>
2013-03-26 11:03:52 +0100

    adding benchmark files, SNU file is empty

---
+++ testcases/real-testcases/SNUbenchmarks.scala
@@ -0,0 +1,16 @@
+
+// Benchmarks from here:
+// http://www.cprover.org/goto-cc/examples/snu.html
+// Original page is not available any more.
+
+// Some of these benchmarks have been used in the Mixed Abstractions paper.
+// It is however unclear exactly which assertions they tried to prove,
+// they only said they were checking for fp exceptions.
+
+
+// For our purpose these need be made functional
+object SNUBenchmarks {
+
+  
+
+}
