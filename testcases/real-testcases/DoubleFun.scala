6405f842548a319ddb788d6a21812adabac65372
Eva Darulova <eva.darulova@epfl.ch>
2013-03-27 17:31:17 +0100

    test examples

--- testcases/real-testcases/DoubleFun.scala
+++ testcases/real-testcases/DoubleFun.scala
@@ -3,15 +3,51 @@ import leon.NumericUtils._
 
 object DoubleFun {
 
-  //def literal(): Double = 4.5
+  def wrongSignature1(x: Double, y: Double): Boolean = {
+    require(x >= -5 && x <= 5 && y >= -5 && y <= 5)
+    if (x * y < 7.0) true
+    else false
+  } ensuring (res => res)
 
-  def arithmetic(x: Double, y: Double, z: Double): Double = {
+  def wrongSignature2(x: Boolean, y: Double): Double = {
+    require(y >= -5 && y <= 5)
+    if (x) y*y
+    else y*y*y
+  } ensuring (res => (res > 0 && absRoundoff(res) <= 1e-10))
+
+  def noPost(x: Double, y: Double): Double = {
+    require(x >= -5 && x <= 5 && y >= -5 && y <= 5)
+    (x + y) * x * y
+  }
+
+  def noPre(x: Double, y: Double): Double = {
+    (x + y) * x * y
+  } ensuring (res => (res > 0 && absRoundoff(res) <= 1e-10))
+
+
+  def arithmeticFull(x: Double, y: Double, z: Double): Double = {
     require(x >= -5 && x <= 5 && y >= -5 && y <= 5 && z >= -5 && z <= 5)
-    val err = absRoundoff(3.5)
     x * y + z / (y - x) * (-x) + 4.5
-  } ensuring (res => (absRoundoff(res) <= 1e-7 && res <= 3.4))
+  } ensuring (res => (absRoundoff(res) <= 1e-7 && res <= 30.4))
 
-  /*def ifThenElse(x: Double, y: Double, z: Double): Double = {
+  // Should be handled with inlining
+  def arithmeticWithVal(x: Double, y: Double): Double = {
+    require(x >= -5 && x <= 5 && y >= -5 && y <= 5)
+    val temp = x * y
+    temp * temp
+  } ensuring (res => (absRoundoff(res) <= 1e-7 && res <= 30.4))
+
+  def ifThenElse(x: Double, y: Double, z: Double): Double = {
+    require(x >= -5 && x <= 5 && y >= -5 && y <= 5 && z >= -5 && z <= 5)
+
+    if (x > 10.0) {
+      (z - x) * (x - y) * (y - z)
+    } else {
+      ((x+(y+z)) * (z-(x-y)) * (z+(x-y)) * (x+(y-z)))
+    }
+  } ensuring (res => (res > 0 && absRoundoff(res) <= 1e-10))
+ 
+  def ifThenElseWithVal(x: Double, y: Double, z: Double): Double = {
     require(x >= -5 && x <= 5 && y >= -5 && y <= 5 && z >= -5 && z <= 5)
     val s = (x + y + z) / 2.0
 
@@ -21,5 +57,5 @@ object DoubleFun {
       ((x+(y+z)) * (z-(x-y)) * (z+(x-y)) * (x+(y-z))) / 4.0
     }
   } ensuring (res => (res > 0 && absRoundoff(res) <= 1e-10))
-  */
+  
 }
