dd9a35ebaa55b48ff81471b1f1ad5b654d2cd664
Eva Darulova <eva.darulova@epfl.ch>
2013-04-03 14:38:28 +0200

    bounds extraction

--- src/main/scala/leon/numerics/Analyser.scala
+++ src/main/scala/leon/numerics/Analyser.scala
@@ -5,18 +5,49 @@ import purescala.Common._
 import purescala.Definitions._
 import purescala.Trees._
 
-import TreeOps._
+import ceres.common.{Rational, RationalInterval}
 
 class Analyser(reporter: Reporter) {
   //class VerificationCondition(val inputs: Map[Expr, RationalInterval], val expr: Expr,
   //val output: RationalInterval, val absRoundoff: Rational) {
+
+  case class PartialInterval(lb: Option[Double], ub: Option[Double]) {
+    def isDefined: Boolean = (lb, ub) match {
+      case (Some(d1), Some(d2)) => true
+      case _ => false
+    }
+
+    def checkAndMerge(other: PartialInterval, varName: Variable): PartialInterval = {
+      val lwrBnd = this.lb match {
+        case Some(v1) => other.lb match {
+            case Some(v2) =>
+              reporter.error("Found two lower bounds for " + varName)
+              None
+            case None => this.lb
+          }
+        case None => other.lb
+      }
+      val upBnd = this.ub match {
+        case Some(v1) => other.ub match {
+          case Some(v2) =>
+            reporter.error("Found two upper bounds for " + varName)
+            None
+            case None => this.ub
+        }
+        case None => other.ub
+      }
+      PartialInterval(lwrBnd, upBnd)
+    }
+  }
+
+  val emptyInterval = PartialInterval(None, None)
 
 
   def generateVCs(funDef: FunDef): Seq[VerificationCondition] = {
     var vcs: Seq[VerificationCondition] = Seq.empty
   
     val funName = funDef.id.name
-    reporter.info("Analysing function " + funName + "...")
+    reporter.info("-----> Analysing function " + funName + "...")
     
     val pre = funDef.precondition
     val post = funDef.postcondition
@@ -27,34 +58,75 @@ class Analyser(reporter: Reporter) {
     //  - we can handle the body
     //  - we have something to prove
     if (post.isEmpty) {
-      reporter.warning("no post, nothing to prove")
+      reporter.info("no post, nothing to prove")
       Seq.empty
-    } else if (pre.isEmpty){
-      reporter.warning("no pre, unconstraint bounds")
+    } else if (pre.isEmpty) {
+      reporter.info("no pre, no bounds")
       Seq.empty
     } else {
-      val bounds = extractVariableBounds(pre.get)
+      val bounds = double2rational(extractVariableBounds(pre.get))
+      reporter.info("found variable bounds: " + bounds)
       // TODO: check that all bounds have been defined
-      if (bounds.isEmpty) {
-        reporter.warning("unconstraint bounds")
-        Seq.empty
-      } else if(!isMathFunction(body)) {
-        reporter.warning("function we cannot handle")
+      // or maybe postpone till we know what we need.
+      val outputRange: Option[RationalInterval] = None
+      val rndoff: Option[Rational] = None
+      if (outputRange.isEmpty && rndoff.isEmpty) {
+        reporter.warning("nothing to prove")
         Seq.empty
       } else {
         // everything seems to be fine
-        val (outputRange, rndoff) = getGoal(post.get)
-        if (outputRange.isEmpty && rndoff.isEmpty) {
-          reporter.warning("nothing to prove")
-          Seq.empty
-        } else {
-          // everything seems to be fine
-
-          Seq(VerificationCondition(funDef, bounds, body, outputRange, rndoff))
-        }
+        Seq(VerificationCondition(funDef, bounds, body, outputRange, rndoff))
       }
     }
   }
+
+  // This will need some partial bounds
+  // For now only accept bound given as follows:
+  // ... && x <= 8.9 && 7.8 <= x && ...
+  private def extractVariableBounds(expr: Expr):
+    Map[Variable, PartialInterval] = expr match {
+    case LessEquals(FloatLiteral(lwrBnd), x @ Variable(name)) =>
+      Map(x -> PartialInterval(Some(lwrBnd), None))
+    case LessEquals(x @ Variable(name), FloatLiteral(uprBnd)) =>
+      Map(x -> PartialInterval(None, Some(uprBnd)))
+    case LessEquals(IntLiteral(lwrBnd), x @ Variable(name)) =>
+      Map(x -> PartialInterval(Some(lwrBnd), None))
+    case LessEquals(x @ Variable(name), IntLiteral(uprBnd)) =>
+      Map(x -> PartialInterval(None, Some(uprBnd)))
+
+    case GreaterEquals(FloatLiteral(uprBnd), x @ Variable(name)) =>
+      Map(x -> PartialInterval(None, Some(uprBnd)))
+    case GreaterEquals(x @ Variable(name), FloatLiteral(lwrBnd)) =>
+      Map(x -> PartialInterval(Some(lwrBnd), None))
+    case GreaterEquals(IntLiteral(uprBnd), x @ Variable(name)) =>
+      Map(x -> PartialInterval(None, Some(uprBnd)))
+    case GreaterEquals(x @ Variable(name), IntLiteral(lwrBnd)) =>
+      Map(x -> PartialInterval(Some(lwrBnd), None))
+
+    case And(exprs) =>
+      var map: Map[Variable, PartialInterval] = Map.empty
+      for (e <- exprs) {
+        val map2 = extractVariableBounds(e)
+        map = map ++ map2.map{ case (k, v) =>
+          k -> v.checkAndMerge(map.getOrElse(k, emptyInterval), k)}
+      }
+      map
+
+    case _ => Map.empty
+  }
+
+
+  private def double2rational(m: Map[Variable, PartialInterval]):
+    Map[Variable, RationalInterval] = {
+      m.collect { case (k, v) if (v.isDefined) =>
+        k -> RationalInterval(Rational(v.lb.get), Rational(v.ub.get))}
+  }
+
+  // for now only get one for the resulting expression
+  /*def extractAbsRoundoff(expr: Expr): Option[Rational] = expr match {
+    case LessEquals(AbsRound)
+    return None
+  }*/
 
 
 }
