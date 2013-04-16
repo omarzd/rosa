fbde674ed52f396c3d9bf73ef2433ae8127bce1c
Eva Darulova <eva.darulova@epfl.ch>
2013-04-16 11:18:35 +0200

    option to analyse only selected functions

--- src/main/scala/leon/numerics/CertificationPhase.scala
+++ src/main/scala/leon/numerics/CertificationPhase.scala
@@ -7,22 +7,32 @@ import purescala.Trees._
 import purescala.TreeOps._
 import purescala.TypeTrees._
 
+import scala.collection.mutable.{Set => MutableSet}
+
 object CertificationPhase extends LeonPhase[Program,CertificationReport] {
   val name = "Certification"
   val description = "Floating-point certification"
+
+  override val definedOptions: Set[LeonOptionDef] = Set(
+    LeonValueOptionDef("functions", "--functions=f1:f2", "Limit verification to f1, f2,...")
+  )
 
   /*
     Since we can have more than one VC for each function, we need to make sure
     that we cover everything, i.e. we don't show only a subpart.
   */
-  def generateVerificationConditions(reporter: Reporter, program: Program):
+  def generateVerificationConditions(reporter: Reporter, program: Program,
+    functionsToAnalyse: Set[String]):
     List[VerificationCondition] = {
 
     var allVCs: Seq[VerificationCondition] = Seq.empty
+    val analysedFunctions: MutableSet[String] = MutableSet.empty
 
     val analyser = new Analyser(reporter)
 
-    for(funDef <- program.definedFunctions.toList) {
+    for(funDef <- program.definedFunctions.toList
+            if (functionsToAnalyse.isEmpty || functionsToAnalyse.contains(funDef.id.name))) {
+      analysedFunctions += funDef.id.name
 
       if (funDef.body.isDefined) {
         // for now, this only generate 1 VC per function, i.e. we accept only
@@ -30,6 +40,10 @@ object CertificationPhase extends LeonPhase[Program,CertificationReport] {
         allVCs ++= analyser.generateVCs(funDef)
       }
     }
+
+    val notFound = functionsToAnalyse -- analysedFunctions
+    notFound.foreach(fn => reporter.error("Did not find function \"" + fn + "\" though it was marked for analysis."))
+
     allVCs.toList
   }
 
@@ -50,9 +64,18 @@ object CertificationPhase extends LeonPhase[Program,CertificationReport] {
   def run(ctx: LeonContext)(program: Program): CertificationReport = {
     val reporter = ctx.reporter
 
+    var functionsToAnalyse = Set[String]()
+
+    for (opt <- ctx.options) opt match {
+      case LeonValueOption("functions", ListValue(fs)) =>
+        functionsToAnalyse = Set() ++ fs
+
+      case _ =>
+    }
+
     reporter.info("Running Certification phase")
 
-    val vcs = generateVerificationConditions(reporter, program)
+    val vcs = generateVerificationConditions(reporter, program, functionsToAnalyse)
     reporter.info("Generated " + vcs.size + " verification conditions")
     checkVerificationConditions(reporter, vcs, ctx, program)
   }
