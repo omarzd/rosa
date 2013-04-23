5863e63c0145b44eef143dbfd3c3b825d4dc76bf
Eva Darulova <eva.darulova@epfl.ch>
2013-04-23 09:59:55 +0200

    simulation test results

--- testcases/ceres-testcases/Debug.scala
+++ testcases/ceres-testcases/Debug.scala
@@ -8,7 +8,11 @@ object Debug {
     require(-5 <= x && x <= 5 && -5 <= y && y <= 5)
     2*x*x - 1.05*x*x*x*x + (x*x*x*x*x*x)/6 + x*y + y*y
   } ensuring (res => res <= 2.357)
+  
 
-
-
+  /*def test(x: Double): Double = {
+    require(x*x == 10*x - 34)
+    x
+  } ensuring (res => res >= 0)
+  */
 }
