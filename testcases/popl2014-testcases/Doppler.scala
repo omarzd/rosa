f35a7422f8cb6b88a22e0e30e19e3547cb9ccae2
Eva Darulova <eva.darulova@epfl.ch>
2013-07-01 19:58:41 +0200

    ...

--- testcases/popl2014-testcases/Doppler.scala
+++ testcases/popl2014-testcases/Doppler.scala
@@ -4,30 +4,30 @@ import Real._
 
 object Doppler {
 
+
   def doppler1(u: Real, v: Real, T: Real): Real = {
-    require(-100 <= u && u <= 100 && 20 <= v && v <= 20000 &&
-     -30 <= T && T <= 50 && noise(u, 1e-7) && noise(v, 1e-9) && noise(T, 1e-6))
+    require(u.in(-100, 100) && v.in(20, 20000) && T.in(-30, 50) &&
+      noise(u, 1e-7) && noise(v, 1e-9) && noise(T, 1e-6))
 
     (- (331.4 + 0.6 * T) *v) / ((331.4 + 0.6*T + u)*(331.4 + 0.6*T + u))
 
   } ensuring (res => -137.0 <= res && res <= -0.35 && noise(res, 1e-4))
 
   def doppler2(u: Real, v: Real, T: Real): Real = {
-    require(-125 <= u && u <= 125 && 15 <= v && v <= 25000 &&
-     -40 <= T && T <= 60 && noise(u, 1e-12) && noise(v, 1e-3) && noise(T, 1e-5))
+    require(u.in(-125, 125) && v.in(15, 25000) && T.in(-40, 60) &&
+      noise(u, 1e-12) && noise(v, 1e-3) && noise(T, 1e-5))
 
     (- (331.4 + 0.6 * T) *v) / ((331.4 + 0.6*T + u)*(331.4 + 0.6*T + u))
 
-  } ensuring (res => -227.5 <= res && res <= -0.05 && noise(res, 1e-12))
+  } ensuring (res => -227.5 < res && res < -0.05 && noise(res, 1e-12))
 
   def doppler3(u: Real, v: Real, T: Real): Real = {
-    require(-30 <= u && u <= 120 && 320 <= v && v <= 20300 &&
-     -50 <= T && T <= 30 && noise(u, 1e-4) && noise(v, 1e-5) && noise(T, 1e-9))
+    require(u.in(-30, 120) && v.in(320, 20300) && T.in(-50, 30) &&
+      noise(u, 1e-4) && noise(v, 1e-5) && noise(T, 1e-9))
 
     (- (331.4 + 0.6 * T) *v) / ((331.4 + 0.6*T + u)*(331.4 + 0.6*T + u))
 
   } ensuring (res => -82.5 <= res && res <= -0.6 && noise(res, 1e-13))
-
 
 
 }
--- testcases/popl2014-testcases/Fixedpoint.scala
+++ testcases/popl2014-testcases/Fixedpoint.scala
@@ -9,26 +9,39 @@ import Real._
 object Fixedpoint {
 
   def rigidBody1(x1: Real, x2: Real, x3: Real): Real = {
-    require(x1 <= 15 && x1 >= -15 && x2 <= 15 && x2 >= -15 &&
-            x3 <= 15 && x3 >= -15 &&
+    require(x1.in(15, -15) && x2.in(-15, -15) && x3.in(-15, -15) &&
             noise(x1, 1e-8) && noise(x2, 1e-8) && noise(x3, 1e-8))
 
     -x1*x2 - 2*x2*x3 - x1 - x3
 
   }
 
+  def rigidBody1_1(x1: Real, x2: Real, x3: Real): Real = {
+    require(x1.in(15, -15) && x2.in(-15, -15) && x3.in(-15, -15) &&
+            noise(x1, 1e-5) && noise(x2, 1e-7) && noise(x3, 1e-6))
+
+    -x1*x2 - 2*x2*x3 - x1 - x3
+
+  }
+
   def rigidBody2(x1: Real, x2: Real, x3: Real): Real = {
-    require(x1 <= 15 && x1 >= -15 && x2 <= 15 && x2 >= -15 &&
-            x3 <= 15 && x3 >= -15 &&
+    require(x1.in(15, -15) && x2.in(-15, 15) && x3.in(-15, 15) &&
             noise(x1, 1e-8) && noise(x2, 1e-8) && noise(x3, 1e-8))
 
     2*x1*x2*x3 + 3*x3*x3 - x2*x1*x2*x3 + 3*x3*x3 - x2
 
   }
 
+  def rigidBody2_1(x1: Real, x2: Real, x3: Real): Real = {
+    require(x1.in(15, -15) && x2.in(-15, 15) && x3.in(-15, 15) &&
+            noise(x1, 1e-5) && noise(x2, 1e-7) && noise(x3, 1e-6))
+
+    2*x1*x2*x3 + 3*x3*x3 - x2*x1*x2*x3 + 3*x3*x3 - x2
+
+  }
+
   def jetEngine(x1: Real, x2: Real): Real = {
-    require(-5 <= x1 && x1 <= 5 && -20 <= x2 && x2 <= 5 &&
-            noise(x1, 1e-8) && noise(x2, 1e-8))
+    require(x1.in(-5, 5) && x2.in(-20, 5) && noise(x1, 1e-8) && noise(x2, 1e-8))
 
     val t = (3*x1*x1 + 2*x2 - x1)
 
@@ -39,5 +52,16 @@ object Fixedpoint {
 
   }
 
+  def jetEngine_1(x1: Real, x2: Real): Real = {
+    require(x1.in(-5, 5) && x2.in(-20, 5) && noise(x1, 1e-10) && noise(x2, 1e-9))
+
+    val t = (3*x1*x1 + 2*x2 - x1)
+
+    x1 + ((2*x1*(t/(x1*x1 + 1))*
+    (t/(x1*x1 + 1) - 3) + x1*x1*(4*(t/(x1*x1 + 1))-6))*
+    (x1*x1 + 1) + 3*x1*x1*(t/(x1*x1 + 1)) + x1*x1*x1 + x1 +
+    3*((3*x1*x1 + 2*x2 -x1)/(x1*x1 + 1)))
+
+  }
 
 }
