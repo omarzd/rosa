bc466d4c5d5329609d6a20018fce23998e99e161
Eva Darulova <eva.darulova@epfl.ch>
2013-07-10 16:51:56 +0200

    ...

--- testcases/popl2014-testcases/InitialExample.scala
+++ testcases/popl2014-testcases/InitialExample.scala
@@ -6,10 +6,8 @@ import Real._
 object InitialExample {
 
   def mainFunction(a: Real, b: Real, c: Real): Real = {
-    require(a.in(1.0, 9.0) && 4.0 <= b && b <= 4.0 && 5.0 <= c && c <= 5.0 &&
-      a + b > c + 0.1 && a + c > b + 0.0001 && b + c > a + 0.1 &&
-      a < c && b < c && a +/- 1e-13)
-    
+    require(a.in(1.5, 4.5) && 4.0 <= b && b <= 4.0 && 5.0 <= c && c <= 5.0 && a +/- 1e-13)
+      
     val area = triangleSorted(a, b, c)
     area
   } ensuring(res => res +/- 1e-11)
@@ -17,8 +15,10 @@ object InitialExample {
 
   
   def triangleSorted(a: Real, b: Real, c: Real): Real = {
-    require(a < c && b < c)
-    
+    require(a.in(1.0, 9.0) && b.in(1.0, 9.0) && c.in(1.0, 9.0) &&
+      a + b > c + 0.1 && a + c > b + 0.0001 && b + c > a + 0.1 &&
+      a < c && b < c)
+
     val discr =
       if (a < b) {
         (c+(b+a)) * (a-(c-b)) * (a+(c-b)) * (c+(b-a))
