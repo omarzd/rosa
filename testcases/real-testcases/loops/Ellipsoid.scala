3d7cd37ca49423faa069b985c7588b7ea1726c1e
Eva Darulova <eva.darulova@epfl.ch>
2013-12-10 15:06:35 +0100

    messing with examples

--- testcases/real-testcases/loops/Ellipsoid.scala
+++ testcases/real-testcases/loops/Ellipsoid.scala
@@ -3,7 +3,7 @@ import Real._
 
 object Ellipsoid {
 
-  def main {
+  /*def main {
 
     step(9.95, 0.0, 10)
 
@@ -11,13 +11,16 @@ object Ellipsoid {
   // to really get that error bound, we'd need to track the error through all computations
   // with our current technique.
   } ensuring( res => res +/- 1e-12)
+  */
 
 
   // We don't have Tuples or Ints, so we return x*x + y*y
   def step(x: Real, y: Real, k: Real): Real = {
-    require(0 <= ~k && ~k <= 10 && ~x*~x + ~y*~y < 100 && ~x >< (-10, 10) && ~y >< (-10, 10))
+    require(0 <= k && k <= 10 && x*x + y*y < 100 && - 10 <= x && x <= 10 &&
+       -10 <= y && y <= 10)
     
-    if (k > 0) {
+    // greater than one, since k should be an integer, but we don't support that yet
+    if (k > 1) {
       val x1 = (9*x - y)/10
       val y1 = (9*y + x)/10
       step(x1,y1,k-1)
@@ -25,5 +28,5 @@ object Ellipsoid {
       x*x + y*y
     }
 
-  } ensuring(res => ~res < 100 && res +/- 1e-12) // && res +/- 1e-12)  
+  } ensuring(res => 0 <= ~res && ~res < 100 && res +/- 1e-12) // && res +/- 1e-12)  
 }
--- testcases/real-testcases/loops/ViktorsExample.scala
+++ testcases/real-testcases/loops/ViktorsExample.scala
@@ -3,14 +3,15 @@ import Real._
 
 object ViktorsExample {
 
-  def test(k: Int, x: Real): Real = {
-    require(x > 1.0)
+  def test(k: Real, x: Real): Real = {
+    require(0.0 < k && k <= 10.0 && 1.0 < x && x > 1.0)
 
     if (k > 0) {
-      test(k-1, x + 0.25) + 0.5
+      // x grows. this is going to be problematic, as we need to satisfy the postcondition
+      test(k - 1, x + 0.25) + 0.5
     } else {
       x
     }
-  }
+  } ensuring (res => res <= 100.0)
 
 }
