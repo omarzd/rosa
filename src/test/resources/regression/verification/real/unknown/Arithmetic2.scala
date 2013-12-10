fe111a7907b1924e0ec5f175b5d6abdb863d43f3
Eva Darulova <eva.darulova@epfl.ch>
2013-12-10 13:50:55 +0100

    fixing regression tests

--- src/test/resources/regression/verification/real/unknown/Arithmetic2.scala
+++ src/test/resources/regression/verification/real/unknown/Arithmetic2.scala
@@ -5,7 +5,7 @@ import Real._
 object Arithmetic1 {
 
   def f(x: Real): Real = {
-    require(x >< (1.0, 2.0))
-    x * x
-  } ensuring (res => ~res <= 4.0)
+    require(x >< (1.0, 2.1))
+    x * x * x
+  } ensuring (res => ~res <= 9.261)
 }
