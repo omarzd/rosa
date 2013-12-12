5f552017d29b305a623276d7a3f8642929b9ea53
Eva Darulova <eva.darulova@epfl.ch>
2013-12-12 14:30:01 +0100

    making ellipsoid proof go through

--- testcases/real-testcases/loops/Ellipsoid.scala
+++ testcases/real-testcases/loops/Ellipsoid.scala
@@ -17,10 +17,12 @@ object Ellipsoid {
   // We don't have Tuples or Ints, so we return x*x + y*y
   def step(x: Real, y: Real, k: Real): Real = {
     require(0 <= k && k <= 10 && x*x + y*y < 100 && - 10 <= x && x <= 10 &&
-       -10 <= y && y <= 10)
+       -10 <= y && y <= 10 &&
+       0 <= ~k && ~k <= 10 && ~x * ~x + ~y * ~y < 100 && - 10 <= ~x && ~x <= 10 &&
+       -10 <= ~y && ~y <= 10)
     
     // greater than one, since k should be an integer, but we don't support that yet
-    if (k > 1) {
+    if (k > 1.1) {
       val x1 = (9*x - y)/10
       val y1 = (9*y + x)/10
       step(x1,y1,k-1)
@@ -28,5 +30,5 @@ object Ellipsoid {
       x*x + y*y
     }
 
-  } ensuring(res => 0 <= ~res && ~res < 100 && res +/- 1e-12) // && res +/- 1e-12)  
+  } ensuring(res => 0 <= ~res && ~res < 100 && res +/- 1e-12)
 }
