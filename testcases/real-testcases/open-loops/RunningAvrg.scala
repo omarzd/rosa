65f95ffcb36b32313c9aed28cdf4bc7a0818f9b2
Eva Darulova <eva.darulova@gmail.com>
2014-03-31 16:33:36 +0200

    some open loop examples

---
+++ testcases/real-testcases/open-loops/RunningAvrg.scala
@@ -0,0 +1,39 @@
+import leon.Real
+import Real._
+
+object RunningAvrg {
+
+  @modelInput                                 // this roundoff thing is probably not going to compile
+  def nextValue: Real = { ??? } ensuring (res => -1200 <= next && next <= 1200 && roundoff(res))
+
+  /**
+    The challenge in this example is that it uses the loop counter
+    in the computation. We may still be able to prove something,
+    since this computation has to hold for any counter.
+  */
+  def mean(currMean: Real, counter: Real): Real = {
+    require(-1200 <= currMean && currMean <= 1200 && loopCounter(counter))
+
+    if (counter < 100) {
+      val nextVal = nextValue
+      val newMean = ((counter - 1) * currMean + nextValue) / counter
+      mean(newMean, counter + 1)
+    } else {
+      currMean
+    }
+
+  } ensuring ( res => res <= 1200.0 && -1200.0 <= res)
+
+  // standard deviation and variance as a running computation
+  // weighted mean
+
+  // inner product (this we'd have to compare against the specialized work)
+
+
+  // online kurtosis algorithm
+  //http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+  // a possible challenge: computing a two-pass covariance
+  //http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+  // the first sum will have already some error
+}
